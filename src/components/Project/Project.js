44da3e4d9fa839281de4ae90c5177ab859bebf66
Breania Harvey <harvey.breania@gmail.com>
2021-12-22 02:41:15 -0600

    images imported to Projects component

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,40 +1,42 @@
 import React, {useState} from 'react';
+const santaImage = require("../../assets/projects/santas-nice-list.jpeg").default
+const drinkImage = require("../../assets/projects/git-me-a-drink.jpeg").default 
 
 function Project() {
     const [projects] = useState([
         {
             title: "Santa's Nice List",
-            image: "./assets/projects/santas-nice-list.jpeg",
+            image: santaImage,
             appLink: "https://santasnicelist.herokuapp.com/",
             repoLink: "https://github.com/harvbcoding/santaslist"
         },
         {
             title: "Git Me a Drink",
-            image: "./assets/projects/git-me-a-drink.jpeg",
+            image: drinkImage,
             appLink: "https://mateusverbar.github.io/gitmeadrink/",
             repoLink: "https://github.com/mateusverbar/gitmeadrink"
         },
         {
             title: "Budget Tracker",
-            image: "./assets/projects/budget-tracker.jpeg",
+            image: require("../../assets/projects/budget-tracker.jpeg").default,
             appLink: "https://harv-budget-tracker.herokuapp.com/",
             repoLink: "https://github.com/harvbcoding/budget-tracker"
         },
         {
             title: "Tech Blog",
-            image: "./assets/projects/tech-blog.jpeg",
+            image: require("../../assets/projects/tech-blog.jpeg").default,
             appLink: "https://harvbtechblog.herokuapp.com/",
             repoLink: "https://github.com/harvbcoding/techblog"
         },
         {
             title: "Food Festival",
-            image: "./assets/projects/food-festival.jpeg",
+            image: require("../../assets/projects/food-festival.jpeg").default,
             appLink: "https://harvbcoding.github.io/food-festival/",
             repoLink: "https://github.com/HarvBCoding/food-festival"
         },
         {
             title: "Note Taker",
-            image: "../assets/projects/note-taker.jpeg",
+            image: require("../../assets/projects/note-taker.jpeg").default,
             appLink: "https://harvbnotetaker.herokuapp.com/",
             repoLink: "https://github.com/HarvBCoding/note-taker/"
         }
@@ -43,12 +45,11 @@ function Project() {
     return (
         <div className="flex-row">
             {projects.map((project) => (
-                <a href={project.appLink}>
+                <a href={project.appLink} key={project.title}>
                   <img 
                     src={project.image}
                     alt={project.title}
                     className="img-thumbnail mx-1"
-                    key={project.title}
                   />
                 </a>
             ))}
