fa10478659facec297f4018769f52793223e255d
Breania Harvey <harvey.breania@gmail.com>
2022-03-21 21:32:46 -0500

    flexbox changes and description changes to About page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,8 +4,6 @@ import { fadeIn } from "react-animations";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import coverPhoto from "../../assets/cover/illustratedpropic.jpeg";
-import { Container } from "@mui/material";
 import "./css/About.css";
 
 const fadeInAnimation = keyframes`${fadeIn}`;
@@ -22,25 +20,25 @@ function About() {
   return (
     <Box sx={{ flexGrow: 1, px: 10, pt: 15, pb: 30 }}>
       <TitleFadeInDiv>
-      <Typography
-        className="title"
-        variant="h1"
-        component="h2"
-        align="center"
-        sx={{ pt: 5 }}
-        gutterBottom
-      >
-        Hello, I'm Bre Harvey
-      </Typography>
-      <Typography
-        className="sub-title"
-        variant="h3"
-        align="center"
-        sx={{ pb: "5rem" }}
-        gutterBottom
-      >
-        Full Stack Developer
-      </Typography>
+        <Typography
+          className="title"
+          variant="h1"
+          component="h2"
+          align="center"
+          sx={{ pt: 5 }}
+          gutterBottom
+        >
+          Hello, I'm Bre Harvey
+        </Typography>
+        <Typography
+          className="sub-title"
+          variant="h3"
+          align="center"
+          sx={{ pb: "5rem" }}
+          gutterBottom
+        >
+          Full Stack Developer
+        </Typography>
       </TitleFadeInDiv>
       <ContentFadeInDiv>
         <Grid
@@ -48,17 +46,13 @@ function About() {
           sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
           alignItems="center"
         >
-          <Grid item xs>
-            <Container>
+          <Grid item md={12} lg={7}>
               <img
-                src={coverPhoto}
-                style={{ height: "50rem", width: "45rem" }}
+                src='https://res.cloudinary.com/dceprxjzq/image/upload/c_fill,h_650,w_650/v1647915086/Portfolio/illustrated-cover-image.jpg'
                 alt="cover"
               />
-            </Container>
           </Grid>
-          <Grid item xs={5}>
-            <Container className="background">
+          <Grid item md={12} lg={5}>
               <Typography
                 className="about-title"
                 variant="h3"
@@ -73,16 +67,16 @@ function About() {
                 variant="subtitle1"
                 align="center"
               >
-                Hi! I am Breania Harvey, though I prefer to be called Bre. Thank
-                you for taking the time to view my work. I am a Saint Louis
-                based full stack developer attending coding bootcamp at
-                Washington Unversity in Saint Louis. I am a detail-oriented,
-                creative person who is excited about web development. I've
-                always been someone who likes to make something beautiful out of
-                nothing and web development gives me the opportunity to make
-                something beautiful every day.
+                Hi! I am Breania Harvey, but you can call me Bre. Thank you for
+                taking the time to view my work. I am a Saint Louis based full
+                stack developer leveraging skill sets developed through an
+                accelerated full stack web development bootcamp with Washington
+                University-Saint Louis. I am a detail-oriented, creative person
+                who is excited about web development. I've always been someone
+                who likes to make something beautiful out of nothing and web
+                development gives me the opportunity to make something beautiful
+                every day.
               </Typography>
-            </Container>
           </Grid>
         </Grid>
       </ContentFadeInDiv>
